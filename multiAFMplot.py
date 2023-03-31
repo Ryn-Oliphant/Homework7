e6e1e209e6852ff1389c023c91a546ce33ff0448
RYNOLIPHANT <rynoliphant@u.boisestate.edu>
2023-03-30 22:25:00 -0600

    Added surface compare plot, need to fix the mean surface

--- multiAFMplot.py
+++ multiAFMplot.py
@@ -2,16 +2,21 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+mean_data = []
 total_data = []
 
 for file in sys.argv[1:]:
 	data = np.loadtxt(file, delimiter=',')
 
-	total_data.append(np.mean(data,axis=0))
+	total_data.append(data)
+	mean_data.append(np.mean(data,axis=0))
+
+	plt.plot(np.mean(data,axis=0),alpha=0.3,color='blue')
 
 total_data = np.array(total_data)
+mean_data = np.array(mean_data)
 
-plt.plot(np.mean(total_data,axis=0))
-plt.title('Overall Mean Surface')
+plt.plot(np.mean(mean_data,axis=0),color='blue')
+plt.title('Surface Compare')
 plt.show()
-plt.savefig('mean_surface.png')
+plt.savefig('surface_compare.png')
